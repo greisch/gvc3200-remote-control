6be37d032d81a167b1f49d062d4e03a73272378c
Guillermo Reisch <greisch@fenf.edu.uy>
2023-08-29 16:13:47 -0300

    added secret TEMPLATE for configuration

---
+++ secret_TEMPLATE_.py
@@ -0,0 +1,24 @@
+
+############################
+#                          #
+# Configuration of GVC3200 #
+#                          #
+############################
+
+### Copy this TEMPLATE to secret.py
+### and change values to meet your requiriments
+# cp secret_TEMPLATE_.py secret.py
+# nano secret.py  ## or use 'vi se
+
+### Username for login in GVC3200 WebPage!
+usuario = "admin"
+
+### Passwd for login whit the previus username
+contrasenia = "superSecreto!!!"
+
+### Base WebServer where is listenign the GVC3200!!
+# note: whitout ending '/'
+# Examples:
+# servidor = "http://MyGVC3200"
+# servicor = "http://200.40.30.124"
+servidor = "http://gvc3200.juanchohouse.dyndns"
